a44d9b06a1f0c40ed4fd662d6fbd4ab333bb2776
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-29 18:11:35 +0530

    programm  to cal simple  interst

--- script.js
+++ script.js
@@ -10,10 +10,27 @@
 
 // accept two input from  the user and output the sum
 
-let firstNum = parseInt( prompt("Enter the first number"));
-let secondNum = parseFloat(prompt("Enter the second number"));
+// let firstNum = parseInt( prompt("Enter the first number"));
+// let secondNum = parseFloat(prompt("Enter the second number"));
 
-let result = firstNum + secondNum;
-console.log( `The sum of ${firstNum} and ${secondNum} is ${result}`)
+// let result = firstNum + secondNum;
+// console.log( `The sum of ${firstNum} and ${secondNum} is ${result}`)
 
 
+
+
+// program to find simple interest
+
+
+
+
+// var calSimple = ()=>{
+
+// var p = parseInt( document.getElementById("principal-amt").value)
+// var r = parseFloat(document.getElementById("interest-rate").value) 
+// var n = parseFloat( document.getElementById("years").value)
+
+// var si = (p*r*n)/100;
+// console.log(`The principal amount is ${p}\nThe interest rate is ${r}\nNumber of years${n}\nThe simple interest is: ${si}`);
+
+// }
