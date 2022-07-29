c013385e6d4d2178020539f6047139b5eb03c4a6
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-29 20:09:22 +0530

    date

--- script.js
+++ script.js
@@ -52,22 +52,63 @@
 
 // program for find he mark persentage
 
+// let submit = document.getElementById("submit")
+// submit.addEventListener("click",()=>{
+//  let mark =parseFloat(document.getElementById("mark").value) 
+ 
+//  if(mark<50){
+//     console.log("Failed")
+//  }else if(mark<60){
+//     console.log("Your grade E")
+//  }else if(mark<70){
+//     console.log("Your grade D")
+//  }else if(mark<80){
+//     console.log("Your grade C")
+//  }else if(mark<90){
+//     console.log("Your grade B")
+//  }else if(mark<=100){
+//     console.log("Your grade A")
+//  }
+
+// })
+
+
+
+
+//  enter the number and output date 
+
+
 let submit = document.getElementById("submit")
 submit.addEventListener("click",()=>{
- let mark =parseFloat(document.getElementById("mark").value) 
- 
- if(mark<50){
-    console.log("Failed")
- }else if(mark<60){
-    console.log("Your grade E")
- }else if(mark<70){
-    console.log("Your grade D")
- }else if(mark<80){
-    console.log("Your grade C")
- }else if(mark<90){
-    console.log("Your grade B")
- }else if(mark<=100){
-    console.log("Your grade A")
- }
+    let number = parseFloat( document.getElementById("number").value);
 
+
+    switch(number){
+        case 1:
+            console.log("Sunday")
+            break;
+
+        case 2:
+            console.log("Monday")
+            break;
+        case 3:
+            console.log("Tuesday")
+            break;
+        case 4:
+            console.log("Wednesday")
+            break;
+        case 5:
+            console.log("Thursday")
+            break;
+        case 6:
+            console.log("Friday")
+            break;
+        case 7:
+            console.log("Saturday")
+            break;
+        default:
+            console.log("Invalid Entry ")
+    }
 })
+
+
