6650b34ac30b042015f6c555d660b466d6be6497
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-29 15:39:58 +0530

    java script  -get character input and display it

---
+++ script.js
@@ -0,0 +1,7 @@
+// java script code  to diplay the user input character
+
+var displayChar = () =>{
+
+    var enterdValue =document.getElementById("enterChar").value;
+    console.log("The enterd character is : "+enterdValue)
+}
