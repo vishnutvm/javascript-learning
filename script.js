625d13b474e44e79502e26d9723a71af3c8e57b4
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-29 16:05:58 +0530

    accept two values form user and display the sum

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // java script code  to diplay the user input character
 
-var displayChar = () =>{
+// var displayChar = () =>{
 
-    var enterdValue =document.getElementById("enterChar").value;
-    console.log("The enterd character is : "+enterdValue)
-}
+//     var enterdValue =document.getElementById("enterChar").value;
+//     console.log("The enterd character is : "+enterdValue)
+// }
+
+
+
+// accept two input from  the user and output the sum
+
+let firstNum = parseInt( prompt("Enter the first number"));
+let secondNum = parseFloat(prompt("Enter the second number"));
+
+let result = firstNum + secondNum;
+console.log( `The sum of ${firstNum} and ${secondNum} is ${result}`)
+
+
