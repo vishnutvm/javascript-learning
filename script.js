e9019918d2fd6d5e4623f39a0d6b5e9c648be7d8
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-31 11:48:49 +0530

    calculator

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-    let num = parseInt(prompt("Enter a number"))
-    var flag;
-    console.log(num)
-    for(i=2;i<=num/2;i++){
-        flag =0;
-    
-        if(num%i == 0){
-            flag=1;
-            break;
-        }
-    }
-    if(flag == 1 && i != 1 && i!= 0){
-        console.log("This number is not a prime number ")
-    }else{
-        console.log("This is a prime number")
-    }
+class Calculaton {
+  addition(num1, num2) {
+    this.result = num1 + num2;
+    this.display();
+  }
+
+  substraction(num1, num2) {
+    this.result = num1 - num2;
+    this.display();
+  }
+  multiplication(num1, num2) {
+    this.result = num1 * num2;
+    this.display();
+  }
+  division(num1, num2) {
+    this.result = num1 / num2;
+    this.display();
+  }
+
+  display() {
+    console.log("result :   "+this.result);
+  }
+}
+
+let main = () => {
+  var num1 = parseInt(prompt("Enter the first num"));
+  var num2 = parseInt(prompt("Enter the second num"));
+
+  var calc = new Calculaton();
+
+  var option = parseInt(
+    prompt(
+      "Enter your choise:\n1-addintion\n2-substraction\n3-multiplication\n4-division"
+    )
+  );
+
+  switch (option) {
+    case 1:
+      calc.addition(num1, num2);
+      break;
+    case 2:
+      calc.substraction(num1, num2);
+      break;
+    case 3:
+      calc.multiplication(num1, num2);
+      break;
+    case 4:
+      calc.division(num1, num2);
+      break;
+  }
+};
+
+
+main()
