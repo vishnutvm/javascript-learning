c95835a2b7aa27ce2537172d876b77fdd5382a02
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-31 18:25:25 +0530

    error making in reversse

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let my_string = prompt("Enter the string");
-
-
+let my_string ;
+my_string =(1234)
 
 try {
+  my_string = my_string.split("").reverse().join(",");
 
-
-my_string = my_string.split("").reverse().join(",")
-
-console.log(`Reversed string is : ${my_string}`)
-  
-  
+  console.log(`Reversed string is : ${my_string}`);
 } catch (error) {
-   console.log(`Error: ${error}`)
-}finally{
-  console.log(`Type of my string is : ${   typeof(my_string)}`  )
+  console.log(`Error: ${error}`);
+} finally {
+  console.log(`Type of my string is : ${typeof my_string}`);
 }
-
