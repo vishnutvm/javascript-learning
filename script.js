55cb2efc825addf3e2d3485947cba2c8ed30186b
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-31 18:21:02 +0530

    reverce using function

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-var library = [
-  {
-    title: "Bill Gates",
-    author: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    title: "Steve Jobs",
-    author: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    author: "Suzanne Collins",
-    readingStatus: false,
-  },
-];
+let my_string = prompt("Enter the string");
 
-for (i = 0; i < library.length; i++) {
-  if (library[i].readingStatus == false) {
-    console.log(
-      `You still need to read ${library[i].title} by ${library[i].author}`
-    );
-  } else {
-    console.log(`Already read ${library[i].title} by ${library[i].author}`);
-  }
+
+
+try {
+
+
+my_string = my_string.split("").reverse().join(",")
+
+console.log(`Reversed string is : ${my_string}`)
+  
+  
+} catch (error) {
+   console.log(`Error: ${error}`)
+}finally{
+  console.log(`Type of my string is : ${   typeof(my_string)}`  )
 }
+
