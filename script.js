d6267dcb21686ec49d61c514901840e96a7e0629
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-31 13:45:53 +0530

    dis array using class obj

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
-var arr1 = new Array();
-var arr2 = new Array();
+class arryGetAndDisplay{
 
-arr1.push([], []);
-arr2.push([], []);
+
+     getArray(size) {
+        console.log("getting  values to array");
+      
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            arr[i][j] = parseInt(prompt("Enter the values to array"));
+          }
+        }
+      
+      }
+      
+      
+       displayArray(size) {
+        document.write("The array : ");
+        document.write("<br>");
+      
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            document.write(arr[i][j] + " ");
+          }
+          document.write("<br>");
+        }
+      }
+      
+
+
+}
+
+
+
+
+var arr = new Array();
+
+arr.push([], []);
+
 
 var size = parseInt(prompt("enter size of the array"));
 
-getArray(size);
-addArray(size);
-displayArray(size);
-
-function getArray(size) {
-  console.log("getting  values to array 1");
-
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      arr1[i][j] = parseInt(prompt("Enter the values to array 1"));
-    }
-  }
-
-  console.log("getting values to array 2");
-
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      arr2[i][j] = parseInt(prompt("Enter the values to array 2"));
-    }
-  }
-}
-
-function addArray() {
-  console.log("Adding the value of 2d arrays");
-
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      arr1[i][j] = arr1[i][j] + arr2[i][j];
-    }
-  }
-}
-
-function displayArray() {
-  document.write("sum of array 1 and array 2");
-  document.write("<br>");
-
-  for (let i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-      document.write(arr1[i][j] + " ");
-    }
-    document.write("<br>");
-  }
-}
+var arrAction = new arryGetAndDisplay()
+arrAction.getArray(size)
+arrAction.displayArray(size)
