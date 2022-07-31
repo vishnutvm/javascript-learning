3cfa84088209aa8f71b741e05c673675d5f83289
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-31 10:43:14 +0530

    prime number

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-var arr1 = new Array()
-var arr2 = new Array()
-
-arr1.push([],[])
-arr2.push([],[])
-var sum =0
-
-var size = parseInt(prompt("enter size of the array")); 
-
-
-console.log("getting  values to array 1")
-
-for(let i=0;i<size;i++){
-    for(let j=0;j<size;j++){
-    arr1[i][j]=parseInt(prompt("Enter the values to array 1")) 
+    let num = parseInt(prompt("Enter a number"))
+    var flag;
+    console.log(num)
+    for(i=2;i<=num/2;i++){
+        flag =0;
+    
+        if(num%i == 0){
+            flag=1;
+            break;
+        }
     }
-}
-
-
-console.log("getting values to array 2")
-
-for(let i=0;i<size;i++){
-    for(let j=0;j<size;j++){
-    arr2[i][j]= parseInt(prompt("Enter the values to array 2")) 
+    if(flag == 1 && i != 1 && i!= 0){
+        console.log("This number is not a prime number ")
+    }else{
+        console.log("This is a prime number")
     }
-}
-
-console.log("Adding the value of 2d arrays")
-
-for(let i =0;i<size;i++){
-    for(let j=0;j<size;j++){
-        sum = arr1[i][j] + arr2[i][j];
-        console.log(sum)
-
-    }
-   
-}
-
